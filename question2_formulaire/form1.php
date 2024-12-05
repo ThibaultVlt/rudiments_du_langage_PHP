7f5db66009544d3512a3cf78d5b27974b57fd191
Thibault <thibvollet@gmail.com>
2024-12-05 10:06:51 +0100

    Fin de l'exercice

--- question2_formulaire/form1.php
+++ question2_formulaire/form1.php
@@ -1,46 +1,19 @@
-<!-- Créez une page form1.php
-
-Placez dans cette page un formulaire auto-référant, deux champs email et password,
-ainsi qu'un bouton de validation de type submit. -->
-<!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <title>Redirection après Traitement</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link rel="stylesheet" href="./style.css"/>
-    </head>
-    <body>
-        <h1>Identifiez-vous</h1>
-            <form method="post" action="form1.php">
-                <input type="text" name="email" placeholder="Email" autocomplete="email">
-                <input type="password" name="password" value="" placeholder="Mot de passe" autocomplete= "current-password">
-                <div class="boutons">
-                    <input type="submit" name="valider" value="Valider">
-                    <input type="reset" id="reset" name="reset" value="Annuler">
-                </div>
-            </form>
-            <?php $msg = ""; ?>
-            <!--  Déterminez si la page est publiée initialement ou s'il s'agit d'un formulaire qui
-            revient (post back) suite à l'action sur le bouton submit. Utilisez pour cela une variable
-            $msg qui affichera « Saisie obligatoire » lors de la première publication de la page. -->
-            <div class="msg"><?php echo $msg; ?></div>
-    </body>
-</html>
 <?php
+    $msg = "";
 //2 - Déterminez si la page est publiée initialement ou s'il s'agit d'un formulaire qui revient (post back) suite à l'action sur le bouton submit. Utilisez pour cela une variable $msg qui affichera « Saisie obligatoire » lors de la première publication de la page.
 // Vérifier que tout les champs du formulaire sont remplis, sinon $msg qui affichera « Saisie obligatoire »
     if (!isset($_POST["valider"])){
+        //Afficher « Saisie obligatoire » lors de la première publication de la page
+        $msg = $msg . " Saisie obligatoire";
         $erreur = true;
     }else{
         $msg = "";
         if(empty($_POST["email"])){
-            $msg = "Saisie obligatoire de l'email";
+            $msg = $msg . "Saisie obligatoire de l'email";
             $erreur = true;
         }
-
         if(empty($_POST["password"])){
-            $msg = "Saisie obligatoire du mot de passe";
+            $msg = $msg . "Saisie obligatoire du mot de passe";
             $erreur = true;
         }
 //3 - Dans le second cas, vérifiez que les champs email et password sont bien renseignés et qu'ils correspondent à des couples de valeurs prises dans le tableau qui suit :
@@ -73,3 +46,30 @@ ainsi qu'un bouton de validation de type submit. -->
         header('Location: form2.php');
     }
 ?>
+<!-- Créez une page form1.php
+Placez dans cette page un formulaire auto-référant, deux champs email et password,
+ainsi qu'un bouton de validation de type submit. -->
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <title>Redirection après Traitement</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" href="./style.css"/>
+    </head>
+    <body>
+        <h1>Identifiez-vous</h1>
+            <form method="post" action="form1.php">
+                <input type="text" name="email" placeholder="Email" autocomplete="email">
+                <input type="password" name="password" value="" placeholder="Mot de passe" autocomplete= "current-password">
+                <div class="boutons">
+                    <input type="submit" name="valider" value="Valider">
+                    <input type="reset" id="reset" name="reset" value="Annuler">
+                </div>
+            </form>
+            <!--  Déterminez si la page est publiée initialement ou s'il s'agit d'un formulaire qui
+            revient (post back) suite à l'action sur le bouton submit. Utilisez pour cela une variable
+            $msg qui affichera « Saisie obligatoire »-->
+            <p class="message"><?php echo $msg ; ?></p>
+    </body>
+</html>
--- question2_formulaire/form2.php
+++ question2_formulaire/form2.php
@@ -5,8 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
     <title>Bienvenue</title>
+    <link rel="stylesheet" href="./style2.css">
 </head>
 <body>
-    
+    <?php
+        if(empty($_COOKIE["email"])){
+            echo "<h2>Vous devez vous identifier</h2>";
+        }else{
+            print "<h2>Boujou " . htmlspecialchars($_COOKIE["email"]) . "</h2>";
+        }
+    ?>
 </body>
 </html>
